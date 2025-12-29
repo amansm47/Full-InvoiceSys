fe3a7838bfe6c33a146d0634a1df3a7daf645248
Aman <amansm2025@gmail.com>
2025-12-29 14:50:18 +0530

    Force CORS headers in serverless function

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,3 +1,16 @@
 const app = require('../server-production');
 
-module.exports = app;
+// Ensure CORS headers are set
+module.exports = (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  
+  return app(req, res);
+};
